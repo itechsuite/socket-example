829824cccff32710327cf7fb4dc4837b0178b550
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-05-05 15:36:17 +0100

    up

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reacts
         </p>
       </header>
     </div>
